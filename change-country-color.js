b30442bd6e2536b60561b4262185db2f8a08e311
MATHEUS NASCIMENTO OLIVEIRA <theusnascimento1999@outlook.com>
2023-02-02 13:16:03 -0300

    Add countries imgs and change color if attack wins

---
+++ change-country-color.js
@@ -0,0 +1,3 @@
+export function changeCountryColor(attackerId, defenseId) {
+    return attackerId, defenseId;
+};
--- start-game.js
+++ start-game.js
@@ -5,3 +5,34 @@
 //Purple
 //Red
 
+//Global variables
+//Three players default
+var playersQuantity = 3;
+var playersColors = ['orange', 'red', 'green'];
+
+//Continents
+var southAmericaCountriesImgs = document.getElementById('southAmerica').getElementsByTagName('img');
+
+
+//Distribute players on the board
+for (let countryImgElement of southAmericaCountriesImgs) {
+    let countryName = countryImgElement.alt;
+    let countryRandomStartColor = getRandomItem(playersColors);
+
+    countryImgElement.src = `images/countries/${countryName}-${countryRandomStartColor}.png`;
+    //Add color to img class
+    countryImgElement.classList.add(`${countryRandomStartColor}`);
+}
+
+// program to get a random item from an array
+function getRandomItem(arr) {
+
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    // get random item
+    const item = arr[randomIndex];
+
+    return item;
+}
+
