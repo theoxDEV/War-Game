86785c975ebdf69de2aef5cb09b7ad7e3e121057
MATHEUS NASCIMENTO OLIVEIRA <theusnascimento1999@outlook.com>
2023-02-10 18:54:19 -0300

    Server settings

--- client/client.js
+++ client/client.js
@@ -3,8 +3,25 @@ const socket = io('http://localhost:3000');
 
 //Callback function fires on the event called 'serverToClient'
 socket.on('serverToClient', (data) => {
-    alert(data);
+    console.log("Player has disconnected: " + data);
 })
+
+const color = [
+    "Red", "Blue", "Yellow", "Gray", "Green", "Purple"
+];
+
+const goal = [
+    "Matar ciano", "Mata cinza", "Matar red"
+];
+
+const randomColor = Math.floor(Math.random() * color.length);
+const randomGoal = Math.floor(Math.random() * goal.length);
+
+//Client sends a message at the moment it got connected with the server
+socket.emit('newPlayer', {
+    color: color[randomColor], 
+    goal: goal[randomGoal]
+});
 
 //Client sends a message at the moment it got connected with the server
 socket.emit('clientToServer', "Hello, server!");
