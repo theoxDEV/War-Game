56cd78d9dee9549ae00af24750fb9df045ef0615
MATHEUS NASCIMENTO OLIVEIRA <theusnascimento1999@outlook.com>
2023-02-14 17:03:38 -0300

    Add forms before game screen

--- scripts/attack-invasion.js
+++ scripts/attack-invasion.js
@@ -1,5 +1,8 @@
 import { changeColors } from './change-country-color.js';
 
+//Establishing a connection with the server on port 5500y
+const socket = io('http://localhost:3000');
+
 
 function attackInvasion(attackerImage, attackerTroops, defenseImage, defenseTroops) {
     let attackerColor = attackerImage.classList[1];
@@ -9,6 +12,9 @@ function attackInvasion(attackerImage, attackerTroops, defenseImage, defenseTroo
     //TransferTroopsAnimation()
     attackerTroops.innerHTML -= 3;
     defenseTroops.innerHTML = 3;
+
+    //Socket client
+    socket.emit('attack')
 }
 
 export { attackInvasion };
